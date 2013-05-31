ae1c0116695848b5343a50c826a74461fbf11ae9
David Warde-Farley <wardefar@iro.umontreal.ca>
2013-05-31 03:32:07 -0400

    Better enumeration script

--- pieceout/fprop_ensemble.py
+++ pieceout/fprop_ensemble.py
@@ -3,7 +3,7 @@ import theano
 from pylearn2.utils.bit_strings import all_bit_strings
 
 
-def fprop_ensemble(mlp, input_scales, arithmetic_mean=False):
+def fprop_ensemble(mlp, input_scales, arithmetic_mean=False, batch=None):
     """
     fprop a single example with all possible dropout masks.
 
@@ -11,7 +11,8 @@ def fprop_ensemble(mlp, input_scales, arithmetic_mean=False):
 
     ONLY USEFUL WITH p = 0.5!
     """
-    batch = mlp.get_input_space().make_batch_theano(batch_size=1)
+    if batch is None:
+        batch = mlp.get_input_space().make_batch_theano(batch_size=1)
     inputs_dim = mlp.get_total_input_dimension(input_scales.keys())
     masks = all_bit_strings(inputs_dim)
     state = batch
@@ -34,18 +35,26 @@ def fprop_ensemble(mlp, input_scales, arithmetic_mean=False):
     return batch, state
 
 
-def compare_ensemble(mlp, dataset, input_scales, arithmetic_mean=False):
-    batch, state = fprop_ensemble(mlp, input_scales, arithmetic_mean)
-    f = theano.function([batch], [state, state > 0.5],
+def compare_ensemble(mlp, dataset, input_scales):
+    batch, state_geo = fprop_ensemble(mlp, input_scales, False)
+    batch, state_ari = fprop_ensemble(mlp, input_scales, True, batch=batch)
+    f = theano.function([batch], [state_geo, state_geo > 0.5,
+                                  state_ari, state_ari > 0.5],
                         allow_input_downcast=True)
-    ensemble_probs = np.empty(dataset.get_design_matrix().shape[0],
-                              dtype='float32')
-    ensemble_preds = np.empty(dataset.get_design_matrix().shape[0],
-                              dtype='uint8')
+    ensemble_probs_geo = np.empty(dataset.get_design_matrix().shape[0],
+                                  dtype='float32')
+    ensemble_preds_geo = np.empty(dataset.get_design_matrix().shape[0],
+                                  dtype='uint8')
+    ensemble_probs_ari = np.empty(dataset.get_design_matrix().shape[0],
+                                  dtype='float32')
+    ensemble_preds_ari = np.empty(dataset.get_design_matrix().shape[0],
+                                  dtype='uint8')
     for i, row in enumerate(dataset.get_design_matrix()):
-        prob, pred = f(row.reshape((1, -1)))
-        ensemble_probs[i] = prob
-        ensemble_preds[i] = pred
+        prob_geo, pred_geo, prob_ari, pred_ari = f(row.reshape((1, -1)))
+        ensemble_probs_geo[i] = prob_geo
+        ensemble_preds_geo[i] = pred_geo
+        ensemble_probs_ari[i] = prob_ari
+        ensemble_preds_ari[i] = pred_ari
     batch = mlp.get_input_space().make_batch_theano()
     g = theano.function([batch], [mlp.fprop(batch), mlp.fprop(batch) > 0.5],
                         allow_input_downcast=True)
@@ -56,8 +65,9 @@ def compare_ensemble(mlp, dataset, input_scales, arithmetic_mean=False):
 
     results = {}
     results['approx_pred_error'] = (approx_pred != targets).mean()
-    results['ensemble_pred_error'] = (ensemble_preds != targets).mean()
-    results['disagreement'] = (approx_pred != ensemble_preds).mean()
+    results['ensemble_pred_error_geo'] = (ensemble_preds_geo != targets).mean()
+    results['ensemble_pred_error_ari'] = (ensemble_preds_ari != targets).mean()
     results['approx_prob'] = approx_prob
-    results['ensemble_prob'] = ensemble_probs
+    results['ensemble_prob_geo'] = ensemble_probs_geo
+    results['ensemble_prob_ari'] = ensemble_probs_ari
     return results
--- pieceout/scripts/fprop_exhaustive.py
+++ pieceout/scripts/fprop_exhaustive.py
@@ -5,14 +5,16 @@ import numpy as np
 from pylearn2.datasets.mnist import MNIST
 from pieceout.datasets.data_subsets import ClassificationSubtask
 from pieceout.datasets.diamond import Diamond
-from pieceout.fprop_ensemble import fprop_ensemble, compare_ensemble
+from pieceout.fprop_ensemble import compare_ensemble
 from pylearn2.utils.serial import load
 
 if sys.argv[1].startswith('mnist23'):
     if sys.argv[2] == 'valid':
-        data = ClassificationSubtask(dataset=MNIST('train', start=50000, stop=60000), classes=[2, 3])
+        data = ClassificationSubtask(dataset=MNIST('train', start=50000,
+                                                   stop=60000), classes=[2, 3])
     elif sys.argv[2] == 'test':
-        data = ClassificationSubtask(dataset=MNIST('train', start=50000, stop=60000), classes=[2, 3])
+        data = ClassificationSubtask(dataset=MNIST('train', start=50000,
+                                                   stop=60000), classes=[2, 3])
     else:
         raise ValueError('WTF')
 elif sys.argv[1].startswith('diamond'):
@@ -27,6 +29,7 @@ else:
 
 model = load(sys.argv[1])
 d = compare_ensemble(model, data, input_scales={'h1': 2., 'y': 2.})
-print sys.argv[1], sys.argv[2], ":", 'ens', d['ensemble_pred_error'], 'w/2', d['approx_pred_error']
+print sys.argv[1], sys.argv[2], ":", 'geo', d['ensemble_pred_error_geo'], 'ari', d['ensemble_pred_error_ari'], 'w/2', d['approx_pred_error']
 np.save(sys.argv[1] + '_' + sys.argv[2] + '_approx.npy', d['approx_prob'])
-np.save(sys.argv[1] + '_' + sys.argv[2] + '_ensemble.npy', d['ensemble_prob'])
+np.save(sys.argv[1] + '_' + sys.argv[2] + '_geo.npy', d['ensemble_prob_geo'])
+np.save(sys.argv[1] + '_' + sys.argv[2] + '_ari.npy', d['ensemble_prob_geo'])
