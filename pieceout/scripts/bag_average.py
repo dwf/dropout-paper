00edc9d093358cddced0d1dcbd84965832d17c62
David Warde-Farley <wardefar@iro.umontreal.ca>
2013-05-31 16:32:19 -0400

    bag averaging

---
+++ pieceout/scripts/bag_average.py
@@ -0,0 +1,14 @@
+import sys
+import numpy as np
+num_models = len(sys.argv[1:-1])
+x = None
+for m in sys.argv[1:-1]:
+    assert "post" in m
+    fprops = np.load(m)
+    if x is None:
+        x = fprops
+    else:
+        x += fprops
+x = x / float(num_models)
+print "Saving to", sys.argv[-1]
+np.save(sys.argv[-1], x)
---
+++ pieceout/scripts/bag_average_geometric.py
@@ -0,0 +1,16 @@
+import sys
+import numpy as np
+num_models = len(sys.argv[1:-1])
+x = None
+for m in sys.argv[1:-1]:
+    assert "pre" in m
+    print "processsing",m
+    fprops = np.load(m)
+    if x is None:
+        x = fprops
+    else:
+        x += fprops
+x = x / float(num_models)
+x = 1. / (1 + np.exp(-x))
+print "Saving to", sys.argv[-1]
+np.save(sys.argv[-1], x)
