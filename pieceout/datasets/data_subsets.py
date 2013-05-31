c18b5b9a3249dcdef31a23104d7f79919a331132
Ian Goodfellow <goodfellow.ian@gmail.com>
2013-05-31 14:49:31 -0400

    get_test_set

--- pieceout/datasets/data_subsets.py
+++ pieceout/datasets/data_subsets.py
@@ -7,6 +7,7 @@ from pylearn2.datasets import DenseDesignMatrix
 class ClassificationSubtask(DenseDesignMatrix):
     def __init__(self, dataset, classes, one_hot=True,
                  simplify_binary=True):
+        self.args = locals()
         X = dataset.get_design_matrix()
         y = dataset.get_targets()
         assert set(classes).issubset(set(y))
@@ -19,6 +20,11 @@ class ClassificationSubtask(DenseDesignMatrix):
             for i, c in enumerate(classes):
                 y_[y_ == c] = i
         super(ClassificationSubtask, self).__init__(X=X_, y=y_)
+
+    def get_test_set(self):
+        dataset = self.args['dataset'].get_test_set()
+        return ClassificationSubtask(dataset, self.args['classes'],
+                self.args['one_hot'], self.args['simplify_binary'])
 
 
 class Bagged(DenseDesignMatrix):
