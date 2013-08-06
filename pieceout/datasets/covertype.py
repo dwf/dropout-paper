7052f09698e338cdfce5012ebebb0102fff3193f
David Warde-Farley <wardefar@iro.umontreal.ca>
2013-08-05 22:00:11 -0700

    CoverType

--- pieceout/datasets/covertype.py
+++ pieceout/datasets/covertype.py
@@ -6,13 +6,14 @@ from load_covertype import load_covertype
 
 class CoverType(DenseDesignMatrix):
     def __init__(self, which_set, standardize_quantitative=True,
-                 separate_types=False, prefix=None):
+                 separate_types=False, prefix=None, one_hot=False):
         if separate_types:
             raise NotImplementedError("This won't work as long as this "
                                       "is a subset of DenseDesignMatrix")
         self._separate_types = separate_types
         self._standardize_quantitative = standardize_quantitative
         self._prefix = prefix
+        self._one_hot = one_hot
         prefix = prefix if prefix is not None else "${PYLEARN2_DATA_PATH}"
         self._raw = load_covertype(
             preprocess(os.path.join(prefix, "covertype")),
@@ -20,11 +21,15 @@ class CoverType(DenseDesignMatrix):
             separate_types=self._separate_types,
             standardize_quantitative=self._standardize_quantitative
         )
+        labels = self._raw[which_set]['labels'] - 1  # 0 - 6, not 1 - 7
+        if one_hot:
+            labels = one_hot(labels, max_label=6)
         super(DenseDesignMatrix, self).__init__(
             X=self._raw[which_set]['features'],
-            y=self._raw[which_set]['labels']
+            y=labels
         )
 
     def get_test_set(self):
         return self.__class__('test', self._standardize_quantitative,
-                              self._separate_types, self._prefix)
+                              self._separate_types, self._prefix,
+                              self._one_hot)
