af3a962dab04e47e490138377bb82299bb504059
David Warde-Farley <wardefar@iro.umontreal.ca>
2013-12-19 20:54:56 -0500

    Improve greatly the fprops script.

--- pieceout/scripts/fprop_exhaustive.py
+++ pieceout/scripts/fprop_exhaustive.py
@@ -1,48 +1,63 @@
 #!/usr/bin/env python
-import sys
+import cPickle
+import argparse
 import numpy as np
-
-from pylearn2.datasets.mnist import MNIST
-from pieceout.datasets.data_subsets import ClassificationSubtask
-from pieceout.datasets.diamond import Diamond
-from pieceout.datasets.covertype import CoverType
+from pylearn2.config.yaml_parse import load as yload
+from pylearn2.utils.timing import log_timing
 from pieceout.fprop_ensemble import compare_ensemble
-from pylearn2.utils.serial import load
+import pandas as pd
 
-if sys.argv[1].startswith('mnist23'):
-    if sys.argv[2] == 'valid':
-        data = ClassificationSubtask(dataset=MNIST('train', start=50000,
-                                                   stop=60000), classes=[2, 3])
-    elif sys.argv[2] == 'test':
-        data = ClassificationSubtask(dataset=MNIST('train', start=50000,
-                                                   stop=60000), classes=[2, 3])
-    else:
-        raise ValueError('WTF')
-elif sys.argv[1].startswith('diamond'):
-    if sys.argv[2] == 'valid':
-        data = Diamond(500, rng=2)
-    elif sys.argv[2] == 'test':
-        data = Diamond(1000, rng=3)
-    else:
-        raise ValueError('WTF')
-elif sys.argv[1].find('svhn') != -1:
-    data = load("/data/lisa/data/gray_svhn_2s_and_3s/test.pkl")
-elif sys.argv[1].startswith('covertype'):
-    if sys.argv[2] == 'valid':
-        data = ClassificationSubtask(CoverType('valid', prefix='/RQexec/wardefar/data'), classes=[0, 1])
-    elif sys.argv[2] == 'test':
-        data = ClassificationSubtask(CoverType('test', prefix='/RQexec/wardefar/data'), classes=[0, 1])
-    else:
-        raise ValueError('WTF')
-else:
-    raise ValueError("WTF")
+import logging
+log = logging.getLogger()
+logging.basicConfig()
+log.setLevel(logging.INFO)
 
-model = load(sys.argv[1])
-d = compare_ensemble(model, data, input_scales={'h1': 2., 'y': 2.})
-print sys.argv[1], sys.argv[2], ":", 'geo', d['ensemble_pred_error_geo'], 'ari', d['ensemble_pred_error_ari'], 'w/2', d['approx_pred_error']
-np.save(sys.argv[1] + '_' + sys.argv[2] + '_approx.npy', d['approx_prob'])
-np.save(sys.argv[1] + '_' + sys.argv[2] + '_geo.npy', d['ensemble_prob_geo'])
-np.save(sys.argv[1] + '_' + sys.argv[2] + '_ari.npy', d['ensemble_prob_ari'])
-np.save(sys.argv[1] + '_' + sys.argv[2] + '_approx_err.npy', d['approx_pred_error'])
-np.save(sys.argv[1] + '_' + sys.argv[2] + '_geo_err.npy', d['ensemble_pred_error_geo'])
-np.save(sys.argv[1] + '_' + sys.argv[2] + '_ari_err.npy', d['ensemble_pred_error_ari'])
+
+def main():
+    parser = argparse.ArgumentParser(description='Collect statistics.')
+    parser.add_argument('infile', nargs='+', type=argparse.FileType('r'),
+                        help="The pickle files to read.")
+    parser.add_argument('-O', '--output', type=argparse.FileType('w'),
+                        help="Output CSV file to write.")
+    args = parser.parse_args()
+    names = [(a.name, a) for a in args.infile]
+    indices = [b[0] for b in names]
+    columns = ['weight_scaling_error', 'geometric_error',
+               'arithmetic_error']
+    df = pd.DataFrame(index=indices, columns=columns)
+    try:
+        dataset = None
+        for i, (name, model_handle) in enumerate(names):
+            with log_timing(log, "Processing %s [%d / %d]" %
+                            (name, i + 1, len(names))):
+                with model_handle as f:
+                    model = cPickle.load(f)
+                if dataset is None:
+                    with log_timing(log, "Loading test set",
+                                    final_msg="Loaded."):
+                        d = None
+                        # HACK HACK HACK
+                        for k in model.monitor._datasets:
+                            if 'valid' in k or '50000' in k:
+                                d = k
+                                break
+                        if d is None:
+                            log.warning("No validation set found, using "
+                                        "first dataset in monitor.")
+                            d = model.monitor._datasets[0]
+                        dataset = yload(d).get_test_set()
+                d = compare_ensemble(model, dataset, input_scales={'h1': 2.,
+                                                                   'y': 2.})
+                df['weight_scaling_error'][name] = d['weight_scaling_error']
+                df['geometric_error'][name] = d['geometric_error']
+                df['arithmetic_error'][name] = d['arithmetic_error']
+                np.save(model_handle.name + '.sca.npy',
+                        d['weight_scaling_output'])
+                np.save(model_handle.name + '.geo.npy', d['geometric_output'])
+                np.save(model_handle.name + '.ari.npy', d['arithmetic_output'])
+    finally:
+        df.to_csv(args.output)
+
+
+if __name__ == "__main__":
+    main()
